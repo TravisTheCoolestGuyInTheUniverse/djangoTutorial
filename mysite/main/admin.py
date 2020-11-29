0286941da90a7bd858fc23dc7b77eb43148797f3
TravisTheCoolestGuyInTheUniverse <sutopu@DESKTOP-I353F3M.localdomain>
2020-11-28 22:53:48 -0500

    completed admin tutorial. investigated functionality that allows database to be edited by superuser via a web browser.

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+#this is the attributes of the User model that will appear 
+#on the admin page as well as the order in which they will appear.
+class UserAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Username/password", {"fields": ["userName", "password"]})
+    ]
+
+#register a model
+admin.site.register(User, UserAdmin)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import time
 
 #/seems like each table in the database is modeled by a class like the one below.
 class User(models.Model):
     userName = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
 

f9f387ee8af4ec890b8ac89e3e51a7d2f189195a
TravisTheCoolestGuyInTheUniverse <sutopu@DESKTOP-I353F3M.localdomain>
2020-12-06 19:39:18 -0500

    added tables to the database and related them via foreign keys. Business rules for the relationship between tables in the database are in the README.

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 from tinymce.widgets import TinyMCE
 from django.db import models
 # Register your models here.
@@ -11,10 +11,16 @@ class TutorialAdmin(admin.ModelAdmin):
     fieldsets = [
         ("title/date published", {"fields": ["title", "datePublished"]}),
         ("content", {"fields": ["content"]}),
+        #not that you would ever want to see/manually chage a foreign key but it's just nice to see.
+        ("foreignKey/slug", {"fields": ["series", "slug"]})
     ]
 
     formfield_overrides = {
         models.TextField: {'widget': TinyMCE()}
     }
 #register a model
+
+admin.site.register(TutorialCategory)
+admin.site.register(TutorialSeries)
 admin.site.register(Tutorial, TutorialAdmin)
+
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,6 +1,40 @@
 from django.db import models
 import time
 
+
+
+
+class TutorialCategory(models.Model):
+    category = models.CharField(max_length=200)
+    categorySummary = models.CharField(max_length=200)
+    categorySlug = models.CharField(max_length=200)
+
+    class Meta:
+        #this is for our admin page so it shows up as Catergories instead of Catergorys
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category
+
+class TutorialSeries(models.Model):
+    series = models.CharField(max_length=200)
+    """
+    a series is related to a category via category foreign key. 
+    on delete tells what to do with related objects when a entity is deleted.
+    For example, if a category was deleted, you could set it to cascade and delete all
+    series related to that category. We obviously don't want to do that in this case so 
+    we set it to the default category.
+    """ 
+    category = models.ForeignKey(TutorialCategory, default=1, verbose_name="Category", on_delete=models.SET_DEFAULT)
+    summary = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Series"
+
+    def __str__(self):
+        return self.series
+
+
 #/seems like each table in the database is modeled by a class like the one below.
 #when making migrations on an actual database, must provide default value on new/added rows.
 #so good database design off the bat is important.
@@ -9,6 +43,9 @@ class Tutorial(models.Model):
     datePublished = models.DateTimeField("date published")
     content = models.TextField(null=True)
 
+    #this connects the Tutorial table with the TutorialSeries table. better documentation on what this does below.
+    series = models.ForeignKey(TutorialSeries, default=1, verbose_name="Series", on_delete=models.SET_DEFAULT)
+    #URL to first tutorial in a tutorial series.
+    slug = models.CharField(max_length=200, default=1)
     def __str__(self):
         return self.title
-
